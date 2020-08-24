5c16872d8d7958fdb140580f6cadce6d83fd5d82
Jamie Brown <jamie@enews@jdb.homelinux.net>
2020-08-24 00:02:55 -0400

    converted to PDO database calls

--- shared.php
+++ shared.php
@@ -2,19 +2,19 @@
 require_once ('./header.php');
 
 if ($_SESSION['logged_in']) {
-        $user = set_get_string_var ('user', $username);
-}
+	$user = set_get_string_var ('user', $username);
+	}
 else {
-        $user = set_get_string_var ('user');
-}
+    $user = set_get_string_var ('user');
+	}
 $display_shared = false;
 
 if (isset ($_GET['user']) && check_username ($user)) {
 	$title = $user . "&#039;s Online Bookmarks";
-}
+	}
 else {
 	$title = "Shared Online-Bookmarks";
-}
+	}
 
 $order = set_get_order ();
 
@@ -78,40 +78,32 @@ if (isset ($_GET['user']) && check_username ($user)) {
     <?php
 
     require_once (ABSOLUTE_PATH . "bookmarks.php");
-    $query = sprintf ("SELECT title, url, description, UNIX_TIMESTAMP(date) AS timestamp, id, favicon
-            FROM bookmark
-            WHERE user='%s'
-            AND childof='%d'
-            AND deleted!='1'
-            AND public='1'
-            ORDER BY $order[1]",
-            $mysql->escape ($user),
-            $mysql->escape ($folderid));
+        $query = "SELECT title, url, description, UNIX_TIMESTAMP(date) AS timestamp, id, favicon FROM bookmark
+                WHERE user=? AND childof=? AND deleted!='1' AND public='1' ORDER BY $order[1]";
+        $args = [$user, $folderid];
+    if ($result = $mysql->query ($query,$args)) {
+        $bookmarks = array ();
 
-//    if ($mysql->query ($query)) {
-    if ($result = $mysql->query ($query)) {
-            $bookmarks = array ();
-//            while ($row = mysql_fetch_assoc ($mysql->result)) {
-            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    array_push ($bookmarks, $row);
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        	array_push ($bookmarks, $row);
             }
-            list_bookmarks ($bookmarks,
-                    false,
-                    false,
-                    $settings['show_bookmark_icon'],
-                    true,
-                    $settings['show_bookmark_description'],
-                    $settings['show_column_date'],
-                    false,
-                    false,
-                    false,
-                    false,
-                    true,
-                    $user);
-    }
+        list_bookmarks ($bookmarks,
+			false,
+			false,
+			$settings['show_bookmark_icon'],
+			true,
+			$settings['show_bookmark_description'],
+			$settings['show_column_date'],
+			false,
+			false,
+			false,
+			false,
+			true,
+			$user);
+	    }
     else {
-            message ($mysql->error);
-    }
+    	message ($mysql->error);
+    	}
 
     ?>
 	
@@ -121,26 +113,22 @@ if (isset ($_GET['user']) && check_username ($user)) {
 <?php
 }
 else {
-		echo '<div id="content" style="height:' .  $table_height . ';">' . "\n";
-        $query = "SELECT user, SUM(bookmarks) AS bookmarks, SUM(folders) AS folders FROM (
-                SELECT user, 1 AS bookmarks, 0 AS folders FROM bookmark WHERE public='1' AND deleted!='1'
-                UNION ALL
-                SELECT user, 0 AS bookmarks , 1 AS folders FROM folder WHERE public='1' AND deleted!='1'
-                ) AS tmp GROUP BY user";
+	echo '<div id="content" style="height:' .  $table_height . ';">' . "\n";
+	$query = "SELECT user, SUM(bookmarks) AS bookmarks, SUM(folders) AS folders FROM (
+              SELECT user, 1 AS bookmarks, 0 AS folders FROM bookmark WHERE public='1' AND deleted!='1'UNION ALL
+              SELECT user, 0 AS bookmarks , 1 AS folders FROM folder WHERE public='1' AND deleted!='1') AS tmp GROUP BY user";
 
-//        if ($mysql->query ($query)) {
-        if ($result = $mysql->query ($query)) {
-//                while ($row = mysql_fetch_object ($mysql->result)) {
-                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                        echo '<p class="shared"><a href="' . $_SERVER['SCRIPT_NAME'] . '?user=' . $row->user . '&folderid=0"><b>' . $row->user . "</b><br>\n";
-                        echo "Shares $row->folders Folders and $row->bookmarks Bookmarks</a></p>\n";
-                }
+	if ($result = $mysql->query ($query)) {
+		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+			echo '<p class="shared"><a href="' . $_SERVER['SCRIPT_NAME'] . '?user=' . $row["user"] . '&folderid=0"><b>' . $row["user"] . "</b><br>\n";
+			echo "Shares ".$row["folders"]." Folders and ".$row["bookmarks"] ."Bookmarks</a></p>\n";
+			}
         }
-        else {
-//                message ($mysql->error);
+	else {
+		message ($mysql->error);
         }
-				echo "</div>";
-}
+	echo "</div>";
+	}
 ?>
 
 	<!-- Main content ends here. -->
