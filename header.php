7de51e4221b7388dc85d94fc6dfc4ef81bb14fdd
Jamie Brown <jamie@enews@jdb.homelinux.net>
2020-08-26 18:59:55 -0400

    original code unchanged

--- header.php
+++ header.php
@@ -3,15 +3,15 @@ define ("ABSOLUTE_PATH", dirname (__FILE__) . "/");
 
 if (extension_loaded ('zlib')) {
     ob_start ('ob_gzhandler');
-}
+	}
 
 require_once (ABSOLUTE_PATH . "lib/webstart.php");
 if (! is_file (ABSOLUTE_PATH . "config/config.php")) {
 	die ('You have to <a href="./install.php">install</a> Online-Bookmarks.');
-}
+	}
 else {
 	require_once (ABSOLUTE_PATH . "config/config.php");
-}
+	}
 require_once (ABSOLUTE_PATH . "lib/mysql.php");
 $mysql = new mysql;
 require_once (ABSOLUTE_PATH . "lib/auth.php");
@@ -37,11 +37,11 @@ require_once (ABSOLUTE_PATH . "lib/login.php");
 
 if (is_file (ABSOLUTE_PATH . "install.php")) {
 	message ('Remove "install.php" before using Online-Bookmarks.');
-}
+	}
 
 if ($display_login_form) {
 	$auth->display_login_form ();
 	require_once (ABSOLUTE_PATH . "footer.php");
-}
+	}
 
 ?>
